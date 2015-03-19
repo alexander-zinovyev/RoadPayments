145984f0c4ebeb553b90d0ef1fc77b23f06dbfda
Alexander <s1deeex@gmail.com>
2015-03-19 16:26:12 +0200

    Removed WelcomeController and configured database

--- payments/app/Http/routes.php
+++ payments/app/Http/routes.php
@@ -10,9 +10,6 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', 'WelcomeController@index');
-
 Route::get('home', 'HomeController@index');
 
 Route::controllers([
