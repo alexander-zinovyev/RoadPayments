b68e83bdb5a51a4b960c2c174691dc6d48814b79
Alexander <s1deeex@gmail.com>
2015-03-19 22:35:43 +0200

    fixed some stuff

--- payments/app/Http/Controllers/Cabinet/MainController.php
+++ payments/app/Http/Controllers/Cabinet/MainController.php
@@ -4,4 +4,11 @@ use App\Http\Controllers\Controller;
 
 class MainController extends Controller {
 	
+	public function __construct() {
+		$this->middleware('auth');
+	}
+
+	public function index() {
+		return view('cabinet/controller');
+	}
 }
