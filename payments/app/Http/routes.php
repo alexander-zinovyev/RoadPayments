6f76ba827d67c0b74043ceada602fc34beb001b7
Alexander <s1deeex@gmail.com>
2015-03-19 17:33:22 +0200

    readded everything

--- payments/app/Http/routes.php
+++ payments/app/Http/routes.php
@@ -10,6 +10,9 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+
+Route::get('/', 'WelcomeController@index');
+
 Route::get('home', 'HomeController@index');
 
 Route::controllers([
--- payments/config/database.php
+++ payments/config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'roadpayments'),
-			'username'  => env('DB_USERNAME', 'roadpayments'),
-			'password'  => env('DB_PASSWORD', 'hackathon'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
