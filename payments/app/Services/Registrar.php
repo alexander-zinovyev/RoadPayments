9a68ad2e74cb188aabd0ccb1b648aa7519a38d49
kiskinvlad <kiskinvlad@gmail.com>
2015-03-19 23:18:05 +0200

    Modifide migration 2

--- payments/app/Services/Registrar.php
+++ payments/app/Services/Registrar.php
@@ -35,7 +35,7 @@ class Registrar implements RegistrarContract {
             'surname' => $data['surname'],
 			'email' => $data['email'],
 			'password' => bcrypt($data['password']),
-            'accountId' => crypt($data['email'],'pmw'),
+            'accountId' => Crypt::encrypt($data['email'],'pmw'),
 		]);
 	}
 
