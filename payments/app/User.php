6db3b9fedb46f5175c4d4e7e9fb1b151a7e5aded
kiskinvlad <kiskinvlad@gmail.com>
2015-03-19 18:13:41 +0200

    Modifide migration

--- payments/app/User.php
+++ payments/app/User.php
@@ -22,7 +22,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'surname', 'email', 'password'];
+	protected $fillable = ['name', 'surname', 'email', 'password', 'accountId'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
