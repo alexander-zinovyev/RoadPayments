528abdeb325b790a46c3e2ddf1535acad357b25c
Ilya <r9b4uk@mail.ru>
2015-03-20 02:39:36 +0200

    fix widget height

--- payments/resources/views/payment/payment.blade.php
+++ payments/resources/views/payment/payment.blade.php
@@ -4,7 +4,7 @@
 @include('sidebar')
 <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 content">
   <div class="payment-wall-widget">
-    <?php echo $widget->getHtmlCode(); ?>
+    <?php echo $widget->getHtmlCode(['height'=>'400','width'=>'100%']); ?>
   </div>
 </div>
 @endsection
