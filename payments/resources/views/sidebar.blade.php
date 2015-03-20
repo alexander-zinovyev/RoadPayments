ffcec6c9f04b32d27b57d368e6f034b2bc5234f1
Alexander <s1deeex@gmail.com>
2015-03-20 10:09:05 +0200

    margarita ivanovna vorob'eva - klassniy rukovoditel 5-go A hotela srat a ya yasnue dni

--- payments/resources/views/sidebar.blade.php
+++ payments/resources/views/sidebar.blade.php
@@ -2,20 +2,26 @@
 <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 sidebar">
   <div class="row">
     <ul class="nav nav-pills nav-stacked">
-      <li class="active"><a href="/home">Home</a></li>
-      <li><a href="/payment/history">History</a></li>
+      <li id="home" class="active"><a href="/home">Home</a></li>
+      <li id="history"><a href="/payment/history">History</a></li>
      <!-- <li><a href="/settings">Settings</a></li>-->
     </ul>
   </div>
 </div>
 <script>
 	
-	$('.active').on('click', function (e) {
-		$(this)
-			.removeClass('active')
+	if (location.pathname === '/payment/history') {
+		$('#history')
+			.addClass('active')
 			.siblings()
-			.addClass('active');
-
-	});
+			.removeClass('active');
+	}
+	
+	if (location.pathname === '/home') {
+		$('#home')
+			.addClass('active')
+			.siblings()
+			.removeClass('active');
+	}
 
 </script>
