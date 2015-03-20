ef4421a5ff99ee57f0173e377894a5a0cae34f17
Ilya <r9b4uk@mail.ru>
2015-03-20 02:31:13 +0200

    added meatspin again

--- payments/resources/views/payment/payment.blade.php
+++ payments/resources/views/payment/payment.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 @include('sidebar')
 <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 content">
-  <?php echo $widget->getHtmlCode(); ?>
+  <div class="payment-wall-widget">
+    <?php echo $widget->getHtmlCode(); ?>
+  </div>
 </div>
 @endsection
